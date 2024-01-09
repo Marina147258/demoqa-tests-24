b973394a14aa309b1455d8c2ea9d2fc3189905fc
Marina Ostapenko <meostapenko@yandex.ru>
2024-01-09 20:52:02 +0300

    changed

--- src/test/java/github/SelenideSoftAssertions.java
+++ src/test/java/github/SelenideSoftAssertions.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-
 public class SelenideSoftAssertions {
     @BeforeAll
     static void beforeAll() {
